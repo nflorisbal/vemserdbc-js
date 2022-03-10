9d1404ecca17a6a7b21032c9ef4016b76b370257
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-09 22:34:11 -0300

    add ex3 lista2

--- aula3/lista2/ex3.js
+++ aula3/lista2/ex3.js
@@ -1,3 +1,11 @@
 // 3) Crie uma função que recebe como parâmetro 2 arrays (quaisquer que sejam)
 //     e retorne uma concatenação destes 2 arrays;
 
+let firstArray = [ 'a', 123, 'teste', null ];
+let secondArray = [ 1000, undefined, -10, 'abc'];
+
+function concatArrays(a, b) {
+    return a.concat(b);
+}
+
+console.log(concatArrays(firstArray, secondArray));
