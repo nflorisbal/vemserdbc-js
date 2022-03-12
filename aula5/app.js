c61341f04d2b6d82eca0635401e2f5a539e1e117
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-12 12:12:12 -0300

    add function textValidate

--- aula5/app.js
+++ aula5/app.js
@@ -37,18 +37,20 @@
   6) Verificar se todos os produtos cadastrados possuem um preço válido;
 */
 
-
+// variáveis globais
 let products = [];
-
+let id = 0;
 
 // função principal
 const start = () => {
+
+    let option;
+
     do {
         option = mainMenu();
-        
-        switch (option){
+        // debugger;
+        switch (option) {
             case '1':
-                console.log('menu 1 abriu');
                 addProduct();
                 break;
             case '2':
@@ -64,29 +66,32 @@ const start = () => {
                 console.log('menu 5 abriu');
                 break;
             case '6':
+            case null:
                 alert('Trabalho concluído. Aplicação encerrada!');
                 break;
             default:
-                alert('Opção inválida. Tente novamente.')
+                alert('Opção inválida. Tente novamente.');
         };
-    } while (option != '6');
+    } while (option !== '6' && option !== null);
 }
 
 // função com a estrutura do menu principal retornando a escolha do usuário
 const mainMenu = () => {
     let option = prompt('Operações disponíveis: \n\n1 - Cadastrar produto\n2 - Excluir produto\n3 - Buscar produto'
-                        + '\n4 - Listar produtos\n5 - Verificar patrimônio\n\n6 - Encerrar');
-    
+        + '\n4 - Listar produtos\n5 - Verificar patrimônio\n\n6 - Encerrar');
+
     return option;
 }
 
+// função para incluir produtos 
 const addProduct = () => {
-    let id;
-    let description;
-    let price;
 
-    
-    
+    let description = textValidate(prompt('Inserir descrição do produto:'));
+    console.log(description);
+    let price = priceValidate(prompt('Inserir preço do produto:'));
+    // console.log(price);
+
+    // id++;
 }
 
 const removeProduct = id => {
@@ -97,5 +102,29 @@ const findProduct = id => {
 
 }
 
+// validação da descrição
+const textValidate = text => {
+    let validate = false;
+    do {
+        if (text === null)
+            return;
+        else if (text !== '' && text.toUpperCase() !== text.toLowerCase())
+            validate = true;
+        else
+            text = prompt('Informação inserida é inválida. Tente novamente (descrição deve conter texto).')
+    } while (!validate);
+
+    return text;
+}
+
+// validação do preço
+const priceValidate = price => {
+    let validate = false;
+    do {
+        
+    }
+}
+
+
 // chamada da função principal
-// start();
+start();
