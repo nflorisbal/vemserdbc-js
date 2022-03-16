9a524f20eb99f17c19c9c2fa4598f27a98588b07
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-16 10:19:38 -0300

    add button function do add new tasks

--- aula7/script.js
+++ aula7/script.js
@@ -17,15 +17,35 @@ const changeTitleTodo = () => {
         titleTodo.textContent = 'To Do:';
 }
 
+const setDivTodo = (element, id) => {
+    element.setAttribute('id', id);
+    element.classList.add('todo-item', 'd-flex', 'border', 'border-secondary', 'justify-content-between', 'mb-2');
+}
+
+const setButton = (element, id, content) => {
+    element.setAttribute('id', id);
+    element.classList.add('btn', 'col-1');
+    element.textContent = content;
+}
+
 const createTodoTask = () => {
     todoCount++;
-
+    const divListTodo = document.getElementById('list-items')   
     const newDivTodo = document.createElement('div');
-    newDivTodo.setAttribute('class', 'todo-item');
-    newDivTodo.appendChild
+    const btnCheck = document.createElement('button');
+    const btnDelete = document.createElement('button');
+    const pTask = document.createElement('p');
+    
+    setDivTodo(newDivTodo, `todo-item-${todoCount}`);
+    setButton(btnCheck, `btn-check-${todoCount}`, 'C');
+    setButton(btnDelete, `btn-delete-${todoCount}`, 'D');
+    pTask.setAttribute('class', 'col-10');
 
 
-
+    divListTodo.appendChild(newDivTodo);
+    newDivTodo.appendChild(btnCheck);
+    newDivTodo.appendChild(pTask);
+    newDivTodo.appendChild(btnDelete);
 
     changeTitleTodo();
 }
