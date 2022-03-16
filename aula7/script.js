6799c839226cfdb154a9d3f3d115418d53acec24
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-16 09:12:52 -0300

    add html structure and button eventlistener

--- aula7/script.js
+++ aula7/script.js
@@ -1,3 +1,38 @@
-const createTodoDiv = () => {
-    
+let todoCount = 0;
+
+const addTodoItem = () => {
+    const btnPlus = document.getElementById('btn-add');
+    btnPlus.addEventListener('click', createTodoTask);
 }
+
+const removeTodoItem = () => {
+
+}
+
+const changeTitleTodo = () => {
+    const titleTodo = document.getElementById('title-todo');
+    if (todoCount === 0)
+        titleTodo.textContent = 'Nenhum "To Do" cadastrado ainda.';
+    else
+        titleTodo.textContent = 'To Do:';
+}
+
+const createTodoTask = () => {
+    todoCount++;
+
+    const newDivTodo = document.createElement('div');
+    newDivTodo.setAttribute('class', 'todo-item');
+    newDivTodo.appendChild
+
+
+
+
+    changeTitleTodo();
+}
+
+
+const init = () => {
+    addTodoItem();
+}
+
+init();
